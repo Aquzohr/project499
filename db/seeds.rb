967016a4446f3c2172d8372b235780ed3eee428e
aquzohr <kampee.expertly01@gmail.com>
2018-02-26 22:00:11 +0700

    add fake infomation

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ require 'faker'
 
 #createBranch
 1.upto(3) do |i|
-	Branch.create(name: Faker::Company.name, address: Faker::Address.building_number+' '+Faker::Address.city+' '+Faker::Address.state+' '+Faker::Address.country+' '+Faker::Address.zip)
+	Branch.create(name: "Powerhouse Fitness Center"+ i.to_s, address: Faker::Address.building_number+' '+Faker::Address.city+' '+Faker::Address.state+' '+Faker::Address.country+' '+Faker::Address.zip)
 end
 
 #create Package
@@ -29,7 +29,7 @@ Staff.create(position: 'Admin', staff_code: 's001', user_id: 1, branch_id: 1)
 
 #Genaral Staff
 User.create(email: 'staff@powerhouse.com', password: 'password', firstname: Faker::Name.first_name, lastname: Faker::Name.last_name, phone: Faker::PhoneNumber.cell_phone, role: 'staff')
-Staff.create(position: 'Staff', staff_code: 's002', user_id: 2, branch_id: 1)
+Staff.create(position: 'Genaral', staff_code: 's002', user_id: 2, branch_id: 1)
 
 #Trainer
 User.create(email: 'trainer@powerhouse.com', password: 'password', firstname: Faker::Name.first_name, lastname: Faker::Name.last_name, phone: Faker::PhoneNumber.cell_phone, role: 'staff')
@@ -39,36 +39,46 @@ Staff.create(position: 'Trainer', staff_code: 's003', user_id: 3, branch_id: 1)
 User.create(email: 'member@powerhouse.com', password: 'password', firstname: Faker::Name.first_name, lastname: Faker::Name.last_name, phone: Faker::PhoneNumber.cell_phone, role: 'member')
 Member.create(member_code: 'm001', start_date: Faker::Date.backward(14), end_date: Faker::Date.forward(23), user_id: 4, nontrainer_package_id: 1)
 
+####################################
+
 #create Announcement
 1.upto(30) do |i|
   Announcement.create(title: Faker::VentureBros.organization, des: Faker::VentureBros.quote, date: Faker::Date.forward(10))
 end
 
-# #create USER
-# 1.upto(30) do |i|
-#    User.create(email: Faker::Internet.unique.free_email, password: 'password', firstname: Faker::Name.first_name, lastname: Faker::Name.last_name, phone: Faker::PhoneNumber.cell_phone, role: 'staff')
-# end
+####################################
 
-# 1.upto(30) do |i|
-#    User.create(email: Faker::Internet.unique.free_email, password: 'password', firstname: Faker::Name.first_name, lastname: Faker::Name.last_name, phone: Faker::PhoneNumber.cell_phone, role: 'member')
-# end
+#create Staff
+1.upto(30) do |i|
+   User.create(email: Faker::Internet.unique.free_email, password: 'password', firstname: Faker::Name.first_name, lastname: Faker::Name.last_name, phone: Faker::PhoneNumber.cell_phone, role: 'staff')
+end
 
-# #create Staff
-# position = ["Sale Representative","Trainer"]
+position = ["Genaral","Trainer", "Admin"]
 
-# 1.upto(30) do |i|
-# 	Staff.create(staff_id: 's'+Faker::Address.building_number, position: position[rand(0..1)], user_id: i, branch_id: rand(1..3))
-# end
+4.upto(33) do |i|
+	Staff.create(staff_code: "s%03d" % i, position: position[rand(0..2)], user_id: i, branch_id: rand(1..3))
+end
 
-# #create Member
-# 31.upto(60) do |i|
-# 	Member.create(member_id: 'm'+Faker::Address.building_number, start_date: Faker::Date.backward(14), end_date: Faker::Date.forward(23), user_id: i, nontrainer_package_id: rand(1..30))
-# end
+###################################
 
-# #create Member
-# 1.upto(30) do |i|
-# 	PackageOccupiedby.create(start_date: Faker::Date.backward(14),end_date: Faker::Date.forward(23),balance_session: 0, trainer_package_id: rand(1..30), member_id: rand(1..30))
-# end
+#create Member
+1.upto(30) do |i|
+   User.create(email: Faker::Internet.unique.free_email, password: 'password', firstname: Faker::Name.first_name, lastname: Faker::Name.last_name, phone: Faker::PhoneNumber.cell_phone, role: 'member')
+end
+
+k=2
+
+34.upto(63) do |i|
+	Member.create(member_code: "m%03d" % k, start_date: Faker::Date.backward(14), end_date: Faker::Date.forward(23), user_id: i, nontrainer_package_id: 1)
+  k=k+1;
+end
+
+###################################
+
+#create Member
+1.upto(30) do |i|
+	PackageOccupiedby.create(start_date: Faker::Date.backward(14),end_date: Faker::Date.forward(23),balance_session: 0, trainer_package_id: rand(1..30), member_id: rand(1..30))
+end
 
 
 
