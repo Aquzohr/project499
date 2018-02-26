a9a43ecfb60d889cb4acdec5642000914abcb386
aquzohr <kampee.expertly01@gmail.com>
2018-02-26 17:16:36 +0700

    new pagination

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -2,7 +2,7 @@ class ReportController < ApplicationController
   def index
 
   	if params[:commit] == "View Serve Report"
-  		@serves = Serve.where(member_id: params[:member_id]).where.not(checkout_time: nil).order(date: :desc, checkout_time: :desc)
+  		@serves = Serve.where(member_id: params[:member_id]).where.not(checkout_time: nil).order(checkout_time: :desc, checkout_time: :desc)
 
       if @serves.blank?
         @serveReport = 1
