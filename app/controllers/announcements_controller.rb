d64708c618779f4f3ea0f150962483e8841e85b2
kampee <kampee.expertly01@gmail.com>
2018-12-11 22:24:10 +0700

    fixed image url

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -88,6 +88,6 @@ class AnnouncementsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def announcement_params
-      params.require(:announcement).permit(:title, :des, :start_datetime, :end_datetime)
+      params.require(:announcement).permit(:title, :des, :start_datetime, :end_datetime, :image_url)
     end
 end
